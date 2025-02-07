39c0e1f44ef7488d875ad865040b505c2ed90b47
Tirth-Chaudhari <tirthchaudhary8080@gmail.com>
2025-02-07 16:07:06 +0530

    scrollTosection function added for scrolling particular section

--- 03-02-2025/Portfolio/portfolio.js
+++ 03-02-2025/Portfolio/portfolio.js
@@ -13,6 +13,42 @@ const projects = document.querySelectorAll(".common-project");
    window.addEventListener("scroll", handleScroll);
    handleScroll(); 
 
+  
+    function scrollToSection(sectionId) 
+    {
+        const nav=document.querySelector('nav');
+        
+        const List=nav.getElementsByTagName('li');
+        
+        for(li of List)
+        {
+            li.classList.remove('active')            
+            if(li.textContent==sectionId)
+            {
+                li.classList.add('active')
+            }
+        }
+        
+        
+        
+        const section = document.getElementById(sectionId);
+        
+        if (section) {
+            const offset = 80; 
+            const sectionPosition = section.getBoundingClientRect().top+window.scrollY - offset;
+            
+            
+            
+            window.scrollTo({
+                top: sectionPosition,
+                behavior: "smooth"
+            });
+        }
+    }
+   
+
+   
+
 
 
   
