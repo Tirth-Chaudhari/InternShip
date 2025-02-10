aa856222ef52e591ba62e3473e90113d9109cce8
Tirth-Chaudhari <tirthchaudhary8080@gmail.com>
2025-02-10 14:07:08 +0530

    learn about class concept in typescript

---
+++ Typescript/class.ts
@@ -0,0 +1,41 @@
+// basic in class 
+
+    // function person(a:string,b:string)
+    // {
+    //     this.name=a;
+    //     this.lname=b;
+    // }
+
+
+    // const p=new person('a','b');
+    // p.obj='no'
+    // p.name='helo'
+    // console.log(p);
+    // const p1=new person('a','b');
+    // console.log(p1);
+
+//inheritance
+
+    // class Person {
+    //     constructor(private firstName: string, private lastName: string) {}
+    //     getFullName(): string {
+    //     return `${this.firstName} ${this.lastName}`;
+    //     }
+    //     describe(): string {
+    //     return `This is ${this.firstName} ${this.lastName}.`;
+    //     }
+    // }
+
+    // class Employee extends Person {
+    //     constructor(
+    //         firstName: string,
+    //         lastName: string,
+    //         private jobTitle: string) {
+    //         super(firstName, lastName);
+    //     }
+    // }
+    // let employee = new Employee('John','Doe','Front-end Developer');
+    // console.log(employee.getFullName());
+    // console.log(employee.describe());
+
+
