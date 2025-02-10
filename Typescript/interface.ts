3406ecd6043e9d45c28af476d37176fdc53c0236
Tirth-Chaudhari <tirthchaudhary8080@gmail.com>
2025-02-10 14:17:43 +0530

    learn about interface concept in typescript

---
+++ Typescript/interface.ts
@@ -0,0 +1,30 @@
+
+
+//interface example
+        // interface Person {
+        //     firstName: string;
+        //     lastName: string;
+        // }
+        // function getPerson(person:Person):string
+        // {
+
+        //         return person.firstName+' '+person.lastName;
+        // }
+        // let john={
+        //     firstName:'John',
+        //     lastName:'Doe'
+        // }
+        // console.log(getPerson(john))
+
+// function types
+
+        // interface StringFormat {
+        //     (fname: string, age: number): string
+        // }
+        // let format:StringFormat;
+        // format=function(fn,age)
+        // {
+        //     return fn+' '+age;
+        // }
+        // console.log(format('john',30));
+        
