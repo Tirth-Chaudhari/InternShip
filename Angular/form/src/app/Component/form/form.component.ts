f2df5f5715a849d5dc0257ed68427e2ef2722ad2
Tirth-Chaudhari <tirthchaudhary8080@gmail.com>
2025-02-11 12:21:25 +0530

    implemented Form with two way databinding

---
+++ Angular/form/src/app/Component/form/form.component.ts
@@ -0,0 +1,48 @@
+import { NgIf } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-form',
+  imports: [FormsModule,NgIf],
+  templateUrl: './form.component.html',
+  styleUrl: './form.component.css'
+})
+export class FormComponent {
+
+  email:string=''
+  phone:string=''
+  password:string=''
+  confirm:string=''
+
+
+
+  Submit()
+  {
+    alert(this.email+''+this.phone+' Register Success')
+    
+  }
+
+  register(registerForm:NgForm)
+  {
+      if(this.password!=this.confirm)
+      {
+        alert('confirm password different')
+      }
+      else if(registerForm.valid)
+      { 
+        alert(this.email+'register successfuly')
+        
+        
+      }
+      else{
+        alert('Enter Valid Entry')
+      }
+  }
+
+  
+
+
+
+
+}
