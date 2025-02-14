19f253dfd3e2277def076303005146e0d276eeff
Tirth-Chaudhari <tirthchaudhary8080@gmail.com>
2025-02-14 18:00:25 +0530

    learn differrent inbuilt pipe like keyvalue,json,number,data etc..

--- Angular/form/src/app/Component/learn-pipe/usepipe/usepipe.component.ts
+++ Angular/form/src/app/Component/learn-pipe/usepipe/usepipe.component.ts
@@ -1,11 +1,11 @@
-import { CurrencyPipe } from '@angular/common';
+import { CurrencyPipe, DecimalPipe, JsonPipe, KeyValuePipe, NgFor } from '@angular/common';
 import { Component, NgModule } from '@angular/core';
 import { CurrencyConvertorPipe } from '../currency-convertor.pipe';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-usepipe',
-  imports: [CurrencyConvertorPipe,FormsModule],
+  imports: [CurrencyConvertorPipe,FormsModule,CurrencyPipe,DecimalPipe,JsonPipe,KeyValuePipe,NgFor],
   templateUrl: './usepipe.component.html',
   styleUrl: './usepipe.component.css'
 })
@@ -14,6 +14,8 @@ export class UsepipeComponent
     your_rate:number=0;
     convert_rate:number=0;
     amount:number=1;
+    object: Object = {foo: 'bar', baz: 'qux', nested: {xyz: 3, numbers: [1, 2, 3, 4, 5]}};
+
 
   
 }
