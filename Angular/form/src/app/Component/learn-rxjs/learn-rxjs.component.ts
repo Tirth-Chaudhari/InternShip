503b2b1795771a22d5bdbd540852370074c4e734
Tirth-Chaudhari <tirthchaudhary8080@gmail.com>
2025-02-18 14:04:49 +0530

    understood differnect operators like debounceTime, distinctUntilChanged, SwtichMap, etc..

--- Angular/form/src/app/Component/learn-rxjs/learn-rxjs.component.ts
+++ Angular/form/src/app/Component/learn-rxjs/learn-rxjs.component.ts
@@ -1,44 +1,76 @@
 import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
-import { AsyncSubject, BehaviorSubject, from, map, Observable, of, ReplaySubject, Subject } from 'rxjs';
+import { FormsModule } from '@angular/forms';
+import { AsyncSubject, BehaviorSubject, debounce, debounceTime, distinctUntilChanged, from, map, Observable, of, ReplaySubject, Subject, switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-learn-rxjs',
-  imports: [NgFor],
+  imports: [NgFor,FormsModule],
   templateUrl: './learn-rxjs.component.html',
   styleUrl: './learn-rxjs.component.css'
 })
 export class LearnRxjsComponent 
 {
     data:any[]=[];
+    key:String='';
+
+    subject:Subject<String>=new Subject<String>();
+
+    constructor()
+    {
+        this.subject.pipe(
+            debounceTime(2000),
+            distinctUntilChanged(),
+            // switchMap((value)=>this.getdata()),
+
+            
+        ).subscribe((value)=>
+        {
+          console.log(value);
+          
+        })
+
+    }
+    // getdata()
+    // {
+    //     console.log('hello');
+    //     return of(1,2,3);
+    // }
     // subject:Subject<number> = new Subject<number>(); 
     //  subject = new BehaviorSubject(0); 
     // subject=new ReplaySubject(2); // 2 is here buffer value
     // subject=new AsyncSubject() 
 
+    onSearch()
+    {
+      this.subject.next(this.key);
+      
+    }
+
     mydata=new Observable((observer)=>
     {
-      setTimeout(()=>
-        {
-            observer.next(1);
-        },1000)
-        // setTimeout(()=>
-        //   {
-        //     observer.error('something wrong')
-        //   },1500)
-      setTimeout(()=>
+        setTimeout(()=>
           {
-            observer.next(2)
-          },2000)
-      setTimeout(()=>
+              observer.next(1);
+          },1000)
+          // setTimeout(()=>
+          //   {
+          //     observer.error('something wrong')
+          //   },1500)
+        setTimeout(()=>
             {
-            observer.next(3)
-            },3000)
-            setTimeout(()=>
+              observer.next(2)
+            },2000)
+        setTimeout(()=>
               {
-              observer.complete();
+              observer.next(3)
               },3000)
+              setTimeout(()=>
+                {
+                observer.complete();
+                },3000)
     })
+   
 
 
     getAsyncData()
@@ -142,12 +174,6 @@ export class LearnRxjsComponent
     // });
     // this.subject.next(5);
     // this.subject.complete();
-
-  
-
-      
-
-
 
     }
 
