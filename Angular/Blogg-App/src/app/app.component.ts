d6360d7bb49b9d51a5331ff2ba5fdb944b84d005
Tirth-Chaudhari <tirthchaudhary8080@gmail.com>
2025-02-19 16:47:34 +0530

    create for blog-application layout,home page

---
+++ Angular/Blogg-App/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './Component/home/home.component';
+import { LayoutComponent } from './Component/layout/layout.component';
+import { BlogPostComponent } from './Component/blog-post/blog-post.component';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet,HomeComponent,LayoutComponent,BlogPostComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'Blogg-App';
+}
