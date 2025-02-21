64366ac32e959e945b35c805233940bf9e0bdf85
Tirth-Chaudhari <tirthchaudhary8080@gmail.com>
2025-02-21 10:23:07 +0530

    created loader and subscribe loader service

--- Angular/Blogg-App/src/app/Component/blog-post/blog-post.component.ts
+++ Angular/Blogg-App/src/app/Component/blog-post/blog-post.component.ts
@@ -11,21 +11,26 @@ import { distinctUntilChanged, switchMap, toArray } from 'rxjs';
 })
 export class BlogPostComponent {
        Data: NewsModel[] = [];   //all Data
-
        Trending: NewsModel[]=[]; // Trending news
+
        categories = ['Business', 'Entertainment', 'General', 'Science', 'Sports', 'Technology']
        selectedCategory: string = 'Business'
+
+       loader:boolean=false;
+       loader_trend:boolean=false;
 
        constructor(private blogService: BlogService) {
 
               // for All blog service searchTerm here
 
               this.blogService.searchTerm.pipe(
+                    
                      distinctUntilChanged(),
                      switchMap((term)=> this.blogService.getAllBlogs(term))
               ).subscribe((results)=>
               {
                      this.Data=results.articles.slice(0,10)
+                     this.blogService.onload.next(false);
               })
 
               // for searchCountry  Trending News here
@@ -36,11 +41,21 @@ export class BlogPostComponent {
               ).subscribe((results)=>
               {
                      this.Trending=results.articles
+                     this.blogService.onload_trend.next(false);
+                     
               })
 
-
+              this.blogService.onload.subscribe((load)=>
+              {
+                     this.loader=load;
+              })
+              this.blogService.onload_trend.subscribe((load)=>
+              {
+                     this.loader_trend=load;
+              })
 
        }
+ 
        ngOnInit()
        {
               this.blogService.onSearch('apple');
