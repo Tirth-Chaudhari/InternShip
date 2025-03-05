2e599d4c4c2ede2ed00d031f445444f4fa49ae7d
Tirth-Chaudhari <tirthchaudhary8080@gmail.com>
2025-03-05 18:08:30 +0530

    learn about rxResource with basic api calling

--- Angular/form/src/app/Component/learn-signal/learn-signal.component.ts
+++ Angular/form/src/app/Component/learn-signal/learn-signal.component.ts
@@ -1,43 +1,67 @@
-import { Component, computed, effect, Signal, signal } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { Component, computed, effect, resource, Signal, signal } from '@angular/core';
+import { rxResource } from '@angular/core/rxjs-interop';
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-learn-signal',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './learn-signal.component.html',
   styleUrl: './learn-signal.component.css'
 })
 export class LearnSignalComponent  {
+      private url='https://jsonplaceholder.typicode.com/users'
 
- count=signal(0);
- double=computed(()=>this.count()*2);
-
- countEffect=effect(()=>
-{
-      console.log('count update'+this.count());
+      uid:number=1;
+      userId=signal<number>(1)
+      // user id signal on change detection user data get
       
-})
-dblCountEffect=effect(()=>
-{
-  console.log('double count update'+this.double());
-  
-})
+      constructor(private http:HttpClient)
+      {
+        
+      }
 
-GetCount()
-{
-  console.log(this.count());
-  console.log(this.double());
-  
-  
-}
+      count=signal(0);
+      double=computed(()=>this.count()*2);
+      countEffect=effect(()=>
+      {
+            console.log('count update'+this.count());
+            
+      })
+      dblCountEffect=effect(()=>
+      {
+        console.log('double count update'+this.double());
+        
+      })
+      GetCount()
+      {
+        console.log(this.count());
+        console.log(this.double());
+      }
+      Increment()
+      {
+        this.count.update((prev)=>prev+1);
+      }
 
-Increment()
-{
-  this.count.update((prev)=>prev+1);
-}
+      users=rxResource({
+          request:()=> ({
+            id:this.userId()
+          }),
+          loader:({request})=> this.http.get<any>(`${this.url}/${request.id}`)
+        })
 
+      private eff=effect(()=>
+      {
+          console.log(this.users.status());
+          console.log(this.users.error());
+          console.log(this.users.value());
+          
+      })
 
- 
- 
+      onClick()
+      {
+        this.userId.set(this.uid);
+      }
+      
 
 
 }
