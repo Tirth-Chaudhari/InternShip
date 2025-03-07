8490b7eb2c9546490e503d11e99c424bd54e1680
Tirth-Chaudhari <tirthchaudhary8080@gmail.com>
2025-03-07 18:01:57 +0530

    nothing

--- Learn Git and Github/demoGit/src/app/app.component.ts
+++ Learn Git and Github/demoGit/src/app/app.component.ts
@@ -1,18 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './Component/navbar/navbar.component';
-<<<<<<< HEAD
-
-@Component({
-  selector: 'app-root',
-  imports: [RouterOutlet,NavbarComponent],
-=======
 import { FooterComponent } from './Component/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet,NavbarComponent,FooterComponent],
->>>>>>> feature/footer
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
